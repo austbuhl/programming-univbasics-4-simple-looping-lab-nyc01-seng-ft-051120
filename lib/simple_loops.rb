68eff1b90ed61c303bfc1a0c793b0a5ce23a994f
Austin Buhler <austinbuhler@gmail.com>
2020-04-05 18:52:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -21,3 +21,11 @@ def output_array(array)
     counter += 1
   end
 end
+
+def return_string_array(array)
+  counter = 0
+  while counter < array.length do
+    return array[counter]
+    counter += 1
+  end
+end
