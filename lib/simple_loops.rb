d88e79292fd1067ece04a82908de57f1ceae2936
Austin Buhler <austinbuhler@gmail.com>
2020-04-05 18:47:03 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,7 @@
 def loop_message_five_times(string)
-  puts "string"
-  puts "string"
-  puts "string"
-  puts "string"
-  puts "string"
+  puts string
+  puts string
+  puts string
+  puts string
+  puts string
 end
